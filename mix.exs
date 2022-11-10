7aed57c70f4f6256b2c56452b9e5a8666493c135
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-09 23:56:00 -0300

    adicionado libs credo e argon

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule ExMonApi.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:argon2_elixir, "~> 3.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+
     ]
   end
 
