92deaa9021d62125c5d89bf67ab1579ec58789b9
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-24 21:43:55 -0300

    criado fallback controller

--- lib/ex_mon_api_web/views/error_view.ex
+++ lib/ex_mon_api_web/views/error_view.ex
@@ -1,16 +1,23 @@
 defmodule ExMonApiWeb.ErrorView do
   use ExMonApiWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  def render("400.json", %{result: result}) do
+    %{
+      message: format_errors(result)
+    }
+  end
+
+  defp format_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
   end
 end
