5e33c108e21ce09400736536aa4148b493e442dd
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-26 19:06:24 -0300

    criado rota de delete

---
+++ lib/ex_mon_api/trainer/delete.ex
@@ -0,0 +1,22 @@
+defmodule ExMonApi.Trainer.Delete do
+  @moduledoc false
+
+  alias ExMonApi.{Trainer, Repo}
+  alias Ecto.UUID
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format!"}
+      {:ok, uuid} -> delete(uuid)
+    end
+  end
+
+  defp delete(uuid) do
+    case fetch_trainer(uuid) do
+      nil -> {:error, "Trainer not found!"}
+      trainer -> Repo.delete(trainer)
+    end
+  end
+
+  defp fetch_trainer(uuid), do: Repo.get(Trainer, uuid)
+end
--- lib/ex_mon_api_web/controllers/trainers_controller.ex
+++ lib/ex_mon_api_web/controllers/trainers_controller.ex
@@ -9,6 +9,12 @@ defmodule ExMonApiWeb.TrainersController do
     |> handle_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMonApi.delete_trainer()
+    |> handle_delete(conn)
+  end
+
   defp handle_response({:ok, trainer}, conn) do
     conn
     |> put_status(:created)
@@ -16,4 +22,12 @@ defmodule ExMonApiWeb.TrainersController do
   end
 
   defp handle_response({:error, _changeset} = error, _conn), do: error
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
+  defp handle_delete({:error, _reason} = error, _conn), do: error
 end
