a2f35c51affe343ce6b76e13759fe13ff4ca8d7f
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-26 22:37:21 -0300

    adicionado httpclient tesla ao projeto

--- mix.exs
+++ mix.exs
@@ -45,8 +45,8 @@ defmodule ExMonApi.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:argon2_elixir, "~> 3.0"},
+      {:tesla, "~> 1.4"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
-
     ]
   end
 
