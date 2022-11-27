068a0ba6b57de5359a7b51fe8762e0a26853ea79
Marcelo Ceolin <fxsobr@gmail.com>
2022-11-26 22:50:40 -0300

    criado client http

--- lib/ex_mon_api/poke_api/client.ex
+++ lib/ex_mon_api/poke_api/client.ex
@@ -8,5 +8,10 @@ defmodule ExMonApi.PokeApi.Client do
   def get_pokemon(name) do
     "/pokemon/#{name}"
     |> get()
+    |> handle_get()
   end
+
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get({:ok, %Tesla.Env{status: 404}}), do: {:error, "Pokemon not found!"}
+  defp handle_get({:error, _reason} = error), do: error
 end
